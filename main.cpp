5c594d56630893d4b6d6b13c92bdafd24a7cb2cb
Afa Cheng <afa@renaissancerobot.com>
2017-03-28 20:41:52 +0800

    init. cl good to go

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+ *
+ * RENAISSANCE ROBOT LLC CONFIDENTIAL
+ * __________________
+ *
+ *  [2017] RENAISSANCE ROBOT LLC
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Renaissance Robot LLC and its suppliers, if any. The intellectual and
+ * technical concepts contained herein are proprietary to Renaissance Robot LLC
+ * and its suppliers and may be covered by U.S. and Foreign Patents, patents in
+ * process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from Renaissance Robot LLC.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
